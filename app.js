374c247f0519f02a92784b0c7d756baf4e7fee92
Ezequiel carrizo <ezequielce490@gmail.com>
2022-02-01 11:01:29 -0300

    added first exercise

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 //Change the version of this program in package.json to 1.1.0
 //For all the excersices Postman or Thunder Client is recommended.
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const ApiData = require('./data.json');//should require the data.json file
+const ApiData = require("./data.json"); //should require the data.json file
 app.use(express.json());
 
-app.get('/spells/:id', (req, res) => {
-    //should respond with the spell with the corresponding id value from data.json    
+app.get("/spells/:id", (req, res) => {
+  const id = Number(req.params.id);
+  console.log(id);
+  if (id > 0 && id < 73) {
+    const spell = ApiData.spells.find((spell) => spell.id === id);
+    console.log(spell);
+    res.status(200).json(spell);
+  } else {
+    res.status(404).json({ message: "id not found" });
+  }
+  //should respond with the spell with the corresponding id value from data.json
 });
 
-app.get('/characters', (req, res) => {
-    //Should use query params to filter the hogwartsHouse and hogwartsStudent
+app.get("/characters", (req, res) => {
+  //Should use query params to filter the hogwartsHouse and hogwartsStudent
 });
 
-app.post('/spells', (req, res) => {
-    //Should recive spell data from request body.
-    //Should validate that the properities "id", "spell" and "use" are present in the body
-    //Response should be {"operation": "add spell", "status": "accepted"} with status 200 if all the valid properities are present
-    //Response should be {"operation": "add spell", "status": "refused"} with status 400 if there is any properitie missing.
+app.post("/spells", (req, res) => {
+  //Should recive spell data from request body.
+  //Should validate that the properities "id", "spell" and "use" are present in the body
+  //Response should be {"operation": "add spell", "status": "accepted"} with status 200 if all the valid properities are present
+  //Response should be {"operation": "add spell", "status": "refused"} with status 400 if there is any properitie missing.
 });
 
 app.listen(port, () => {
-    console.log(`Express server started at port ${port}`)
+  console.log(`Express server started at port ${port}`);
 });
