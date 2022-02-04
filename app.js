bdf6482ece867977c854a497b45745b092e0e6d9
JesicaRodriguez <yesicanoemirodriguez56@gmail.com>
2022-02-03 22:34:21 -0300

    guardar cambios final

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/data/:id', (req, res) => {
     const id = Number(req.params.id);
   console.log(id);
   if (id > 0 && id < 73) {
-    const data = ApiData.data.find((data) => data.id === id);
+    const data = ApiData.spells.find((data) => data.id === id);
     res.status(200).json(data);
   } else {
     res.status(404).json({ message: "id not found" });
@@ -23,15 +23,36 @@ app.get('/data/:id', (req, res) => {
      
 });
 
+//Should use query params to filter the hogwartsHouse and hogwartsStudent
 app.get('/characters', (req, res) => {
-    //Should use query params to filter the hogwartsHouse and hogwartsStudent
+  const word = req.query.word;
+  let response = [];
+  if (word) {
+    response = ApiData.characters.filter(
+      (apple) => apple.hogwartsStudent && apple.hogwartsHouse === word
+    );
+  } else {
+    response = ApiData.characters.filter((apple) => apple.hogwartsStudent);
+  }
+  if (response.length > 0) {
+    res.status(200).json(response);
+  } else {
+    res.status(404).json({ House: word, characters: "Not Found" });
+  }
+   
 });
 
-app.post('/spells', (req, res) => {
-    //Should recive spell data from request body.
+//Should recive spell data from request body.
     //Should validate that the properities "id", "spell" and "use" are present in the body
     //Response should be {"operation": "add spell", "status": "accepted"} with status 200 if all the valid properities are present
     //Response should be {"operation": "add spell", "status": "refused"} with status 400 if there is any properitie missing.
+app.post('/spells', (req, res) => {
+  const { id, spell, use } = req.body;
+  if (!id || !spell || !use) {
+    res.status(400).json({ operation: "add spell", status: "refused" });
+  } else {
+    res.status(200).json({ operation: "add spell", status: "accepted" });
+  }
 });
 
 app.listen(port, () => {
