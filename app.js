13931854ad93c8b48c85754c37328c8955dc4ee2
Efren <exg22@hotmail.com>
2022-02-06 22:02:51 -0300

    resuelto endpoint get /characters y post /spells

--- app.js
+++ app.js
@@ -9,15 +9,40 @@ app.use(express.json());
 app.get('/spells/:id', (req, res) => {
     //should respond with the spell with the corresponding id value from data.json    
     const { id } = req.params;
-    const response = ApiData.spells.find((data) => {return Number(id) === data.id});
+    const response = ApiData.spells.find((data) => { return Number(id) === data.id });
     res.json(response);
 });
 
 app.get('/characters', (req, res) => {
     //Should use query params to filter the hogwartsHouse and hogwartsStudent
+    const { hogwartsStudent, hogwartsHouse } = req.query;
+    const a = (v) => { return v === "false" || v === "null" || v === "NaN" || v === "undefined" || v === "0" ? false : !!v; }
+    if (hogwartsHouse && hogwartsStudent) {
+        const response = ApiData.characters.filter((data) => { return (hogwartsHouse === data.hogwartsHouse && a(hogwartsStudent) === data.hogwartsStudent) });
+        res.json({ response });
+    } else {
+        res.status(400).json({
+            code: 'bad_request',
+            message: 'Invalid request, check your query params',
+            severity: 'LOW'
+        })
+    }
 });
 
 app.post('/spells', (req, res) => {
+    const { id, spell, use } = req.body;
+
+    if (id && spell && use) {
+        res.status(200).json({
+            operation: 'add spell',
+            status: 'accepted'
+        });
+    } else {
+        res.status(400).json({
+            operation: 'add spell',
+            status: 'refused'
+        });
+    }
     //Should recive spell data from request body.
     //Should validate that the properities "id", "spell" and "use" are present in the body
     //Response should be {"operation": "add spell", "status": "accepted"} with status 200 if all the valid properities are present
