61235c560cecfac988cba412a3b5e42474f5c7c3
Exequiel Ramirez <ramirezj.exequiel@gmail.com>
2022-11-19 18:49:45 -0300

    feat: add new feature

--- app.js
+++ app.js
@@ -8,10 +8,25 @@ app.use(express.json());
 
 app.get('/spells/:id', (req, res) => {
     //should respond with the spell with the corresponding id value from data.json    
+    const {id} = req.params;
+    const spell = ApiData.spells.find((spell)=> spell.id == id);
+
+    res.json(spell);
 });
 
 app.get('/characters', (req, res) => {
     //Should use query params to filter the hogwartsHouse and hogwartsStudent
+    const {hogwartsHouse, hogwartsStudent} = req.query;
+    if(!hogwartsStudent && !hogwartsHouse){
+        res.json(ApiData.characters);
+    }
+    const result = ApiData.characters.filter((character) => {
+
+        return character.hogwartsStudent == JSON.parse(hogwartsStudent) ||
+                character.hogwartsHouse == hogwartsHouse;
+    })
+
+    res.json(result);
 });
 
 app.post('/spells', (req, res) => {
@@ -19,6 +34,19 @@ app.post('/spells', (req, res) => {
     //Should validate that the properities "id", "spell" and "use" are present in the body
     //Response should be {"operation": "add spell", "status": "accepted"} with status 200 if all the valid properities are present
     //Response should be {"operation": "add spell", "status": "refused"} with status 400 if there is any properitie missing.
+    const data = req.body;
+    if(!data.id || !data.spell || !data.use){
+        res.status(400).json({
+            operation: "add spell", 
+            status: "refused"
+        });
+    }
+   ApiData.spells.push(data);
+
+    res.json({
+        operation: "add spell", 
+        status: "accepted"
+    })
 });
 
 app.listen(port, () => {
