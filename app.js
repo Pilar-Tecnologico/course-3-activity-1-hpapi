1cdd6f61e43d5a8db522c95b63e719bb66d72511
Ezequiel <ezequielce490@gmail.com>
2022-11-20 22:06:38 -0300

    second exercise

--- app.js
+++ app.js
@@ -20,6 +20,28 @@ app.get('/spells/:id', (req, res) => {
 
 app.get('/characters', (req, res) => {
   //Should use query params to filter the hogwartsHouse and hogwartsStudent
+
+  const { hogwartsHouse, hogwartsStudent } = req.query;
+
+  let characters;
+
+  try {
+    if (hogwartsStudent === 'true') {
+      characters = ApiData.characters.filter(
+        (student) => student.hogwartsHouse === hogwartsHouse && student.hogwartsStudent === true,
+      );
+    } else {
+      characters = ApiData.characters.filter(
+        (student) => student.hogwartsStudent === false && student.hogwartsHouse === hogwartsHouse,
+      );
+    }
+
+    if (characters.length < 1) throw new Error('cant find characters');
+
+    res.status(200).json(characters);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 });
 
 app.post('/spells', (req, res) => {
