94142af3045a4c20b5681279c5c1ab6f908a0035
MasterDjmov <masterdjmov@gmail.com>
2022-11-20 21:31:00 -0300

    Punto 2 characters find

--- app.js
+++ app.js
@@ -15,6 +15,18 @@ app.get('/spells/:id', (req, res) => {
 
 app.get('/characters', (req, res) => {
     //Should use query params to filter the hogwartsHouse and hogwartsStudent
+    let { hogwartsStudent, hogwartsHouse } = req.query;
+
+    if(hogwartsHouse && hogwartsStudent){
+        const filtro = ApiData.characters.filter(Character => (Character.hogwartsStudent == (hogwartsStudent === 'true') && Character.hogwartsHouse === hogwartsHouse));
+        res.send(filtro);    
+    }
+    else{
+        res.status(400)
+        res.json({
+            message: 'Home and student not found'
+        })
+    }
 });
 
 app.post('/spells', (req, res) => {
